fcd1b705e7b8198dbd54e8a7d15ebfc00dce105a
Erick Briones <erickalan518@gmail.com>
2023-07-19 21:36:58 -0600

    Creating loading modal

--- components/users/UserBox.tsx
+++ components/users/UserBox.tsx
@@ -6,6 +6,7 @@ import { User } from '@prisma/client';
 import axios from 'axios';
 
 import Avatar from '../ui/Avatar';
+import LoadingModal from '../LoadingModal';
 
 interface UserBoxProps {
 	user: User;
@@ -28,22 +29,27 @@ const UserBox: FC<UserBoxProps> = ({ user }) => {
 	}, [router, user]);
 
 	return (
-		<div
-			onClick={handleClick}
-			className="w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer"
-		>
-			<Avatar user={user} />
-			<div className="min-w-0 flex-1">
-				<div className="focus:outline-none">
-					{/* <span className="absolute inset-0"></span> */}
-					<div className="flex justify-between items-center mb-1">
-						<p className="text-sm font-medium text-gray-900">
-							{user.name}
-						</p>
+		<>
+		{isLoading && (
+			<LoadingModal />
+		)}
+			<div
+				onClick={handleClick}
+				className="w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer"
+			>
+				<Avatar user={user} />
+				<div className="min-w-0 flex-1">
+					<div className="focus:outline-none">
+						{/* <span className="absolute inset-0"></span> */}
+						<div className="flex justify-between items-center mb-1">
+							<p className="text-sm font-medium text-gray-900">
+								{user.name}
+							</p>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
